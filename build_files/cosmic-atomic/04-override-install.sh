ae62d806d08e81d242a9936a1c947327470ce9fe
iitzrohan <t.rohan1903@gmail.com>
2025-07-12 20:08:07 +0530

    Use niri git

--- build_files/cosmic-atomic/04-override-install.sh
+++ build_files/cosmic-atomic/04-override-install.sh
@@ -4,6 +4,13 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
+dnf5 -y copr enable yalter/niri-git 
+
+# Set higher priority
+dnf5 config-manager setopt copr:copr.fedorainfracloud.org:yalter:niri-git.priority=90
+
 dnf5 -y install niri fcft gtk-layer-shell libmpdclient nanosvg xcb-util-cursor --setopt=install_weak_deps=False
+
+dnf5 -y copr remove yalter/niri-git 
 
 echo "::endgroup::"
